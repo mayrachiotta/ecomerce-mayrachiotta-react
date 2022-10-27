edeeb3f06531004e247d450cd0e4f34a33a20bdd
Mayra Chiotta <mayraa.chiotta@gmail.com>
2022-10-26 21:28:41 -0300

    CheckOut Order

--- src/components/CheckOut/checkout.js
+++ src/components/CheckOut/checkout.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../checkoutform/checkoutform.css"
 import { useParams } from "react-router-dom";
 import { getSingleOrder } from '../../services/firestore';
+import "./../checkoutform/checkoutform.css"
 
 function CheckOut() {
     const { orderid } = useParams();
@@ -11,14 +12,21 @@ function CheckOut() {
     useEffect(() => {
         getSingleOrder(orderid).then((respuestaDatos) => {
             setOrder(respuestaDatos)
+            console.log(order)
         });
     }, [orderid]);
     return (
-        <div>
-            <h2 className="checkout"> ¡Gracias por tu compra! </h2>
-            <div>
-                {JSON.stringify(order)}
+
+        <div className="ordenCompra">
+
+            <h2 className="orderTitle">¡Gracias por tu compra {order?.buyer?.name?.name}!</h2>
+            <div className="orderPedido">
+                <h3>Tu pedido fue procesado:<h5>{order?.buyer?.items?.map(item =>
+                    (<>  <span>{item.count}</span> unidades de <span>{item.description}.</span></>)
+                )}</h5></h3>
+            <h3> Total a pagar:<h5>${order?.buyer?.total}.</h5></h3>
             </div>
+            <h4 className="OrderContact"> Nos pondremos en contacto a la brevedad</h4>
         </div>
     )
 
